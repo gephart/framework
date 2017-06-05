fe5680daa8749b5aeae79aef14756d15236211eb
Michal Katuščák <michal@katuscak.cz>
2017-06-05 16:51:09 +0200

    Redirect to login route if exists

--- src/EventListener/SecurityListener.php
+++ src/EventListener/SecurityListener.php
@@ -4,9 +4,9 @@ namespace Gephart\Framework\EventListener;
 
 use Gephart\EventManager\Event;
 use Gephart\EventManager\EventManager;
-use Gephart\Framework\Line\Controller\LineController;
 use Gephart\Routing\Router;
 use Gephart\Security\Authenticator\Authenticator;
+use Gephart\Security\Configuration\SecurityConfiguration;
 use Gephart\Security\SecurityReader;
 
 class SecurityListener
@@ -21,15 +21,27 @@ class SecurityListener
      * @var SecurityReader
      */
     private $security_reader;
+    /**
+     * @var SecurityConfiguration
+     */
+    private $security_configuration;
+    /**
+     * @var Router
+     */
+    private $router;
 
     public function __construct(
         EventManager $event_manager,
         Authenticator $authenticator,
-        SecurityReader $security_reader
+        SecurityReader $security_reader,
+        SecurityConfiguration $security_configuration,
+        Router $router
     )
     {
         $this->authenticator = $authenticator;
         $this->security_reader = $security_reader;
+        $this->security_configuration = $security_configuration;
+        $this->router = $router;
 
         $event_manager->attach(Router::BEFORE_CALL_EVENT, [$this, "beforeCall"]);
     }
@@ -42,8 +54,15 @@ class SecurityListener
         $must_have_role = $this->security_reader->getMustHaveRole($controller, $action);
 
         if ($must_have_role && !$this->authenticator->isGranted($must_have_role)) {
-            @header('HTTP/1.0 403 Forbidden');
-            throw new \Exception("403 Forbidden");
+            $login = $this->security_configuration->get("login");
+            if ($login) {
+                $url = $this->router->generateUrl($login);
+                @header("location: $url");
+                exit;
+            } else {
+                @header('HTTP/1.0 403 Forbidden');
+                throw new \Exception("403 Forbidden");
+            }
         }
     }
 }
